effdfe087db4676e7c6de83d6e7410ca700b2cfe
unknown <kale_zhang@qq.com>
2017-07-30 14:14:32 +0800

    feat: modify goods module

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { SexNamePipe } from './sex-name.pipe';
 import { TokPipe } from './tok.pipe';
+import { StarCntPipe } from './star-cnt.pipe';
 
 @NgModule({
   imports: [
   ],
   declarations: [
     SexNamePipe,
-    TokPipe
+    TokPipe,
+    StarCntPipe
   ],
   exports:[
     SexNamePipe,
-    TokPipe
+    TokPipe,
+    StarCntPipe
   ]
 })
 export class PipesModule { }
